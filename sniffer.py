daa20c3f0262d59cab7f4a80f177e360b9004892
Schuka48 <artem.shchukin.2000@mail.ru>
2022-09-19 21:55:37 +0300

    add TCP/UDP packet handler

--- sniffer.py
+++ sniffer.py
@@ -36,7 +36,7 @@ class EthernetFrame:
         dst_mac, src_mac, proto = struct.unpack('!6s6sH', self.data[:14])
         return self.get_mac_address(dst_mac), self.get_mac_address(src_mac), socket.ntohs(proto)
 
-    def get_ethernet_data(self):
+    def get_data(self):
         return self.data[14:]
 
     def __str__(self):
@@ -72,7 +72,7 @@ class IPPacket:
     def __str__(self):
         return f"\tIP Packet:\n\t\t\tSource: {self.src_address}\tDestination: {self.dst_address}\t Proto: {self.proto}"
 
-    def get_ip_data(self):
+    def get_data(self):
         return self.data
 
 
@@ -85,7 +85,51 @@ class ICMP:
         self.data = raw_data[16:]
 
     def __str__(self):
-        return f"Checksum: {hex(self.checksum)}\tData: {self.data}\tLen: {len(self.data)}"
+        return f"\t\t\t\tICMP\t\tChecksum: {hex(self.checksum)}\tData: {self.data}\tLen: {len(self.data)}"
+
+
+class TCP:
+    def __init__(self, raw_data):
+        tcp_header = struct.unpack("!HHLLH", raw_data[:14])
+        self.src_port = tcp_header[0]
+        self.dst_port = tcp_header[1]
+        self.sequence = tcp_header[2]
+        self.acknowledgment = tcp_header[3]
+        self.offset_reserved_flags = tcp_header[4]
+        self.offset = (self.offset_reserved_flags >> 12) * 4
+        self.reserved = (self.offset_reserved_flags >> 8) & 15
+        self.flag_urg = (self.offset_reserved_flags & 32) >> 5
+        self.flag_ack = (self.offset_reserved_flags & 16) >> 4
+        self.flag_psh = (self.offset_reserved_flags & 8) >> 3
+        self.flag_rst = (self.offset_reserved_flags & 4) >> 2
+        self.flag_syn = (self.offset_reserved_flags & 2) >> 1
+        self.flag_fin = self.offset_reserved_flags & 1
+        self.data = raw_data[self.offset:]
+
+    def get_data(self):
+        return self.data
+
+    def __str__(self):
+        return f"\t\tTCP\t Src_Port: {self.src_port}\tDst_Port: {self.dst_port}\n" \
+               f"\t\t\t Sequence: {self.sequence}\tAcknowledgment: {self.acknowledgment}\n" \
+               f"\t\t\t Flags URG:{self.flag_urg} ACK:{self.flag_ack} PSH:{self.flag_psh} " \
+               f"RST:{self.flag_syn} SYN:{self.flag_syn} FIN:{self.flag_fin}"
+
+
+class UDP:
+    def __init__(self, raw_data):
+        udp_header = struct.unpack("!HHHH", raw_data[:8])
+        self.src_port = udp_header[0]
+        self.dst_port = udp_header[1]
+        self.length = udp_header[2]  # Переменная обозначает длину всего UDP-пакета, учитывая заголовок
+        self.chk_sum = udp_header[3]
+        self.data = raw_data[8:]
+
+    def get_data(self):
+        return self.data
+
+    def __str__(self):
+        return f"\t\tUDP\t Src_Port: {self.src_port}\tDst_Port: {self.dst_port}\tLength: {self.length}"
 
 
 class PacketSniffer:
@@ -109,13 +153,23 @@ class PacketSniffer:
                 raw_data = self.sniff_socket.recvfrom(65535)[0]
                 ethernet_frame = EthernetFrame(raw_data)
                 if ethernet_frame.proto == 8:
-                    ip_packet = IPPacket(ethernet_frame.get_ethernet_data())
-                    if ip_packet.dst_address == "10.33.102.104":
+                    ip_packet = IPPacket(ethernet_frame.get_data())
+                    if ip_packet.proto == 1:
+                        icmp_packet = ICMP(ip_packet.get_data())
                         print(ethernet_frame)
                         print(ip_packet)
-                        if ip_packet.proto == 1:
-                            icmp_packet = ICMP(ip_packet.get_ip_data())
-                            print(icmp_packet)
+                        print(icmp_packet)
+                    elif ip_packet.proto == 6:
+                        tcp_packet = TCP(ip_packet.get_data())
+                        if ip_packet.src_address == "192.168.148.129":
+                            print(ethernet_frame)
+                            print(ip_packet)
+                            print(tcp_packet)
+                    elif ip_packet.proto == 17:
+                        udp_packet = UDP(ip_packet.get_data())
+                        print(ethernet_frame)
+                        print(ip_packet)
+                        print(udp_packet)
         except KeyboardInterrupt:
             print("\nStop Sniffer ...")
             print("Good By!")
